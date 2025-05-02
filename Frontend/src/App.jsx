5fd7e001fd84ca5d374d5ffb7f809c0668ff449b
Bhavesh Kabdwal <bhaveshk2210@gmail.com>
2025-05-03 01:15:12 +0530

    Frontend Initialised

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-    <div classname='font-bold'>Smart-Agriculture-Support-System</div>
-      
+      <div>Demo App</div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
